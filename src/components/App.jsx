22896029468e6c8a9ade622c2d4d3ec3f243dafa
SvKost <skost0634610557@gmail.com>
2024-02-18 18:19:05 +0200

    Create project

---
+++ src/components/App.jsx
@@ -0,0 +1,31 @@
+// import { useState } from "react";
+import Profile from "./Profile";
+import "./App.css";
+
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+}
+
+export default App;
