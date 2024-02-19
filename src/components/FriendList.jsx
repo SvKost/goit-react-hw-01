65d8bbcb9e15478dcee1a7d8e9874a8979c34ef9
SvKost <skost0634610557@gmail.com>
2024-02-19 21:58:55 +0200

    Create FriendList.jsx

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,12 @@
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {/* Кількість li залежить від кількості об'єктів в масиві */}
+      <li>
+        <FriendListItem />
+      </li>
+    </ul>
+  );
+};
+
+export default FriendList;
